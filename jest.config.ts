5b473f4293ee1e3139b5f67c231dc597dd3f401b
gustavobgt <gustavo_bgt97@hotmail.com>
2022-10-04 08:47:34 -0300

    chore: configure jest and rtl with swc

---
+++ jest.config.ts
@@ -0,0 +1,38 @@
+export default {
+	clearMocks: true,
+	collectCoverage: true,
+	coverageDirectory: 'coverage',
+	coverageProvider: 'v8',
+	testEnvironment: 'jsdom',
+	transform: {
+		'^.+\\.(t|j)sx?$': [
+			'@swc/jest',
+			{
+				jsc: {
+					parser: {
+						syntax: 'typescript',
+						tsx: true,
+						decorators: true,
+					},
+					keepClassNames: true,
+					transform: {
+						legacyDecorator: true,
+						decoratorMetadata: true,
+						react: {
+							runtime: 'automatic',
+						},
+					},
+				},
+				module: {
+					type: 'es6',
+					noInterop: false,
+				},
+			},
+		],
+	},
+	setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+	moduleNameMapper: {
+		'\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/__mocks__/fileMock.js',
+	},
+};
