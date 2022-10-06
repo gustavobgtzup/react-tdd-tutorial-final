a9f2c65bd8399fc67223ab9ade0cc22f7f72aa11
gustavobgt <gustavo_bgt97@hotmail.com>
2022-10-06 06:58:00 -0300

    fix: fix spacing generated values

--- src/infrastructure/theme/spacing.ts
+++ src/infrastructure/theme/spacing.ts
@@ -1,5 +1,5 @@
 const multiplier = 0.4;
 const N = 20;
-const base = [...Array(N + 1).keys()].slice(1);
+const base = [...Array(N + 1).keys()].slice(0);
 
-export const space = base.map(value => `${value * multiplier}rem`);
+export const space = base.map(value => `${(value * multiplier).toFixed(1)}rem`);
